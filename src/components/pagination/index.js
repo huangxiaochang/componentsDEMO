4b5d882629e4369ba8287ecd7cf2c568a46bb6c0
huang <12969754315@qq.com>
2017-11-02 18:03:23 +0800

    modify pagination component

--- src/components/pagination/index.js
+++ src/components/pagination/index.js
@@ -32,13 +32,15 @@ export default {
 				}
 			],
 			pageCount: 0,
-			activeSize: 0,
+			active: 1,//当前页
+			activePage: [],//动态页
+			preDot: false,//向前的点
+			nextDot: false,//向后的点
 		}
 	},
 	watch: {
 		// 页面条数改变
 		pageSize() {
-			console.log(this.pageSize)
 			this.$emit('pageSizeChange',this.pageSize)
 		}
 	},
@@ -51,35 +53,119 @@ export default {
 			// 初始化每页条数
 			if (this.size <= 15) {
 				this.pageSize = 15
-				this.activeSize = 0
 			}
 			else if (this.size <= 30) {
 				this.pageSize = 30
-				this.activeSize = 1
 			}
 			else if (this.size <= 50) {
 				this.pageSize = 50
-				this.activeSize = 2
 			}
 			else {
 				this.pageSize = 100
-				this.activeSize = 3
 			}
 			// 获取页数
-			let count = 0
 			this.pageCount = Math.ceil(this.total/this.pageSize)
+			//控制显示的页数
+			if (this.pageCount > 6) {
+				for(let i=2; i<7;i++) {
+					this.activePage.push(i)
+				}
+				this.nextDot = true
+				console.log(this.activePage)
+			}
 		},
 		// 每页条数改变
-		sizeChange(index) {
-			this.activeSize = index
+		sizeChange(size) {
+			
+		},
+		// 当前页的变化
+		pageChange(index) {
+			this.active = index
 		},
 		/*上一页*/
 		prePage() {
-
+			--this.active
+			// 只在页面多于指定显示页数(6)、目前页是最后一页的前4页之前的页数和目前页大于前4页时，进行页面的省略操作
+			if (this.active < this.pageCount - 3 && this.pageCount > 6 && this.active > 3) {
+				this.nextDot = true
+				this.activePage.pop()
+				this.activePage.unshift(this.active-2)
+				if (this.active === 4) {
+					this.preDot = false
+				}
+			}
 		},
-		// 上一页
+		// 下一页
 		nextPage() {
-
+			++this.active
+			// 只在页面多于指定显示页数(6)、目前页是最后一页的前4页之前的页数和目前页大于前4页时，进行页面的省略操作
+			if ( this.active > 4 && this.pageCount > 6 && this.active+2 < this.pageCount) {
+				this.preDot = true
+				this.activePage.shift()
+				this.activePage.push(this.active+2)
+			}
+			// 目前页是最后一页的前4页时，隐藏下一页的省略号
+			if (this.active > this.pageCount - 4) {
+				this.nextDot = false
+			}
+		},
+		// 上一批活动页
+		preActivePage() {
+			this.active -= 5
+			this.activePage = []
+			// 目前页小于最后一页的前4页时，显示下一页的省略号
+			if (this.active < this.pageCount - 4) {
+				this.nextDot = true
+			}
+			else {
+				this.nextDot = false
+			}
+			// 目前页大于最后一页的前4页时，显示下一页的省略号
+			if (this.active >  4) {
+				this.preDot = true
+			}
+			else {
+				this.preDot = false
+			}
+			if (this.active - 2 <= 1) {
+				for(let i = 2; i < 7; i++) {
+					this.activePage.push(i)
+				}
+			}
+			else {
+				for(let i = this.active - 2; i < this.active + 3; i++) {
+					this.activePage.push(i)
+				}
+			}
+		},
+		// 下一批活动页
+		nextActivePage() {
+			this.active += 5
+			this.activePage = []
+			// 目前页小于最后一页的前4页时，显示下一页的省略号
+			if (this.active < this.pageCount - 4) {
+				this.nextDot = true
+			}
+			else {
+				this.nextDot = false
+			}
+			// 目前页大于最后一页的前4页时，显示下一页的省略号
+			if (this.active >  4) {
+				this.preDot = true
+			}
+			else {
+				this.preDot = false
+			}
+			if (this.active + 3 > this.pageCount) {
+				for(let i = this.pageCount - 6 ; i < this.pageCount - 1 ; i++) {
+					this.activePage.push(i)
+				}
+			}
+			else {
+				for(let i = this.active - 2; i < this.active + 3; i++) {
+					this.activePage.push(i)
+				}
+			}
 		}
 	}
 }
--- src/components/pagination/index.vue
+++ src/components/pagination/index.vue
@@ -2,12 +2,24 @@
 	<!-- 分页 -->
 	<div class="pagination-wrape">
 		<select class="page-size" v-model="pageSize">
-			<option class="size-item" v-for="(size,index) in sizeList" :key="size.value" :value="size.value" :class="[index === activeSize ? 'activeSize' : '']" @click="sizeChange(index)">{{size.title}}</option>
+			<option class="size-item" v-for="size in sizeList" :key="size.value" :value="size.value" @click="sizeChange(size)">{{size.title}}</option>
 		</select>
 		<ul class="page-item">
-			<li class="pre item" @click="prePage"><</li>
-			<li class="item" v-for="(item,index) in pageCount">{{index+1}}</li>
-			<li class="next item" @click="nextPage">></li>
+			<button class="pre item" @click="prePage" :disabled="active === 1"><</button>
+			<li class="item" :class="[active === 1 ? 'active' : '']" @click="pageChange(1)">1</li>
+
+			<li class="item preDot" v-if="preDot">
+				<span class="dot">...</span>
+				<span class="pre" @click="preActivePage"><<</span>
+			</li>
+			<li class="item" v-for="item in activePage" :class="[item === active ? 'active' : '']" @click="pageChange(item)">{{item}}</li>
+			<li class="item nextDot" v-if="nextDot">
+				<span class="dot">...</span>
+				<span class="next" @click="nextActivePage">>></span>
+			</li>
+
+			<li class="item" :class="[active === pageCount ? 'active' : '']" @click="pageChange(pageCount)" v-if="pageCount > 6">{{pageCount}}</li>
+			<button class="next item" :disabled="active === pageCount" @click="nextPage">></button>
 		</ul>
 		<span class="total">共{{total}}条</span>
 	</div>
@@ -28,15 +40,21 @@
 		height: 30px;
 		line-height: 30px;
 		vertical-align: top;
+		/*将默认的select选择框样式清除*/
+		// appearance:none;
+		// -moz-appearance:none;
+		// -webkit-appearance:none;
 		.size-item {
 			padding: 5px 0;
 			&:hover {
+				cursor: pointer;
 				background-color: rgba(0, 0, 0, 0.3);
 			}
+			&:checked {
+				background-color: #409EFF;
+			}
 		}
-		.activeSize {
-			background-color: #409EFF;
-		}
+		
 	}
 	.page-item {
 		display: inline-block;
@@ -45,17 +63,56 @@
 		vertical-align: top;
 		margin-left: 10px;
 		font-size: 0;
-		.item {
+		.item, .pre, .next {
 			display: inline-block;
 			font-size: 14px;
 			width: 30px;
 			text-align: center;
+			height: 28px;
 			line-height: 28px;
 			border-right: 1px solid #ccc;
 			color: #878D99;
+			cursor: pointer;
+			vertical-align: top;
+			position: relative;
 			&:last-child {
 				border: none;
 			}
+			.dot, .next, .pre {
+				position: absolute;
+				display: inline-block;
+				width: 100%;
+				height: 100%;
+				top: 0;
+				left: 0;
+			}
+			.dot {
+				z-index: 10;
+				background-color: #fff;
+			}
+			.next, .pre {
+				z-index: 5;
+				background-color: #fff;
+			}
+			
+		}
+		.nextDot:hover .next, .preDot:hover .pre {
+			z-index: 20;
+		}
+		.pre, .next {
+			border: none;
+			border-right: 1px solid #ccc;
+			padding: 0;
+			background-color: #fff;
+			vertical-align: top;
+			outline: none;
+			&:disabled {
+				cursor: not-allowed;
+			}
+		}
+		.active {
+			background-color: #409EFF;
+			color: #fff;
 		}
 	}
 	.total {
